77489acad0582e29ab9a57a802845135a108e293
FernandaRojas152 <FernandaRojas152@hotmail.com>
2020-02-13 14:41:31 -0500

    Adding the message of 'ExceptionNotAttended' that it's going to be thrown when the client it's not attended

--- src/customExceptions/ExceptionNotAttended.java
+++ src/customExceptions/ExceptionNotAttended.java
@@ -1,7 +1,12 @@
 package customExceptions;
 
 public class ExceptionNotAttended extends Exception {
-	public ExceptionNotAttended(String msg) {
-		super();
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ExceptionNotAttended() {
+		super("The client wasn't attended due to he wasn't in the place when he was called");
 	}
 }
--- test/TestEmployee.java
+++ test/TestEmployee.java
@@ -3,7 +3,9 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class TestEmployee {
-
+	public void setupStage1() {
+		
+	}
 	@Test
 	void test() {
 		fail("Not yet implemented");
