ec97dfae8b1de9efa09073ce57d1753d4568d5cd
FernandaRojas152 <FernandaRojas152@hotmail.com>
2020-02-13 00:00:30 -0500

    finishing Client's documentation.

--- src/model/Client.java
+++ src/model/Client.java
@@ -22,10 +22,10 @@ public class Client {
 	//Methods
 	/**
 	 * Constructor's method of class Client
-	 * @param docType -
-	 * @param id -
-	 * @param name
-	 * @param lastName
+	 * @param docType -client's document type.
+	 * @param id -client's document number.
+	 * @param name -client's document name.
+	 * @param lastName -client's document lastName.
 	 */
 	public Client(String docType, String id, String name, String lastName) {
 		this.docType = docType;
@@ -53,48 +53,108 @@ public class Client {
 	}
 	
 	//getters and setters for Client's attributes
+	/**
+	 * Gets the document type of the client
+	 * @return docType -an String that represent the document type
+	 */
 	public String getDocType() {
 		return docType;
 	}
+	
+	/**
+	 * Modifies the document type of the client
+	 * @param docType -the type that it's going to be modificated
+	 */
 	public void setDocType(String docType) {
 		this.docType = docType;
 	}
+	
+	/**
+	 * Gets the document number of the client
+	 * @return id- a String that represents the id.
+	 */
 	public String getId() {
 		return id;
 	}
+	
+	/**
+	 * Modifies the document number of the client
+	 * @param id -the id that it's going to be changed.
+	 */
 	public void setId(String id) {
 		this.id = id;
 	}
+	
+	/**
+	 * Gets the name of the client.
+	 * @return name -a String that represents the name.
+	 */
 	public String getName() {
 		return name;
 	}
+	
+	/**
+	 * Modifies the name of the client.
+	 * @param name- the name that the person has.
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	/**
+	 * Gets the last name of the client
+	 * @return lastName -a String that represents the last name.
+	 */
 	public String getLastName() {
 		return lastName;
 	}
+	
+	/**
+	 * Modifies the last name of the client.
+	 * @param lastName -the last name that it's going to change.
+	 */
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+	
+	/**
+	 * Gets the phone number of the client
+	 * @return phone -a String that represents the phone number.
+	 */
 	public String getPhone() {
 		return phone;
 	}
+	
+	/**
+	 * Modifies the phone number of the client
+	 * @param phone -the phone that the client has.
+	 */
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
+	
+	/**
+	 * Gets the address of the client
+	 * @return direction -a String that represents the address
+	 */
 	public String getDirection() {
 		return direction;
 	}
+	
+	/**
+	 * Modifies the address of the client
+	 * @param direction -the address that the client has input.
+	 */
 	public void setDirection(String direction) {
 		this.direction = direction;
 	}
 	
+	/**
+	 * gets the turn that has been assigned to the client
+	 * @return turn -an object of type Turn that it's the turn that
+	 * the client has in order to be attended.
+	 */
 	public Turn getTurn() {
 		return turn;
-	}
-
-	public void setTurn(Turn turn) {
-		this.turn = turn;
 	}
 }
