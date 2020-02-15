28dec4c812a17ffa7feac63b0b4708e26cbf2142
FernandaRojas152 <FernandaRojas152@hotmail.com>
2020-02-15 15:26:01 -0500

    adding more tests

--- test/TestEmployee.java
+++ test/TestEmployee.java
@@ -7,21 +7,52 @@ import model.Employee;
 /**
  * @version Feb 14th 2020
  * @author Fernanda
- *
+ * Class Employee
  */
 class TestEmployee {
 	private Employee employee;
+	
 	public void setupStage1() throws ExceptionRequiredFields {
 		employee.addClient("CC", "3186170", "Amanda", "Rojas", "4388786", "Cra 12 E #50-45");
 	}
 	
 	@Test
-	void test() throws ExceptionRequiredFields {
+	void testUserDoesntExist() throws ExceptionRequiredFields {
 		setupStage1();
 		
 		assertThrows(expectedType, executable, message)
 		assertEquals(true, validation);
 		
 	}
+	
+	/**
+	@Test
+	void testUserExist() throws ExceptionRequiredFields {
+		setupStage1();
+		
+	}
+	
+	@Test
+	void testNotUserRegistered() throws ExceptionRequiredFields {
+		setupStage1();
+		
+	}
+	
+	//test Search Client
+	@Test
+	void testThereAreClients() throws ExceptionRequiredFields {
+		setupStage1();
+		
+	}
+	*/
+	
+	//test Assign turn
+	
+	
+	//Consult turns
+	
+	
+	
+	//Attends turn
 
 }
